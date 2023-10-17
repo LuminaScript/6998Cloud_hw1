a86da611a58ae318290ceb6dd56286553636126f
Yizhen Zhang <98562104+LuminaScript@users.noreply.github.com>
2023-10-17 16:05:42 -0400

    Update LF0.py

--- LF0.py
+++ LF0.py
@@ -1,20 +1,15 @@
 import boto3
-# Define the client to interact with Lex
 client = boto3.client('lexv2-runtime')
 def lambda_handler(event, context):
     
-    # msg_from_user = event['messages'][0]
-    
-    # change this to the message that user submits on
-    # your website using the 'event' variable
-    msg_from_user = "Hello"
+    msg_from_user = event['messages'][0]['unstructured']['text']
     
     print(f"Message from frontend: {msg_from_user}")
     
     # Initiate conversation with Lex
     response = client.recognize_text(
                                 botId='JGMME1KT7Z', # MODIFY HERE
-                                botAliasId='YBAHAOLNRV', # MODIFY HERE
+                                botAliasId='TSTALIASID', # MODIFY HERE
                                 localeId='en_US',
                                 sessionId='testuser',
                                 text=msg_from_user)
@@ -26,11 +21,14 @@ def lambda_handler(event, context):
         print(response)
         
         resp = {
-        'statusCode': 200,
-        'body': "Hello from LF0!"
+                    'statusCode': 200,
+                    'messages': [
+                        {
+                            'type': "unstructured",
+                            'unstructured': {
+                                'text': msg_from_lex[0]['content']
+                            }
+                        }
+                    ]
         }
-        # modify resp to send back the next question Lex would ask from the user
-        # format resp in a way that is understood by the frontend
-        # HINT: refer to function insertMessage() in chat.js that you uploaded
-        # to the S3 bucket
-    return resp
+        return resp
